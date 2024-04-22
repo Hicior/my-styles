72e167c9dc15974df1bdccdd90cc0508207776cd
Hicior <167181464+Hicior@users.noreply.github.com>
2024-04-22 10:28:57 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,44 @@
-window.onload = function() {
-    alert('Hello! This is a test alert to check JavaScript functionality.');
-};
+var swiper = new Swiper(".slide-content", {
+    slidesPerView: 3,
+    spaceBetween: 25,
+    loop: true,
+    centerSlide: 'true',
+    fade: 'true',
+    grabCursor: 'true',
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+
+    breakpoints:{
+        0: {
+            slidesPerView: 1,
+        },
+        520: {
+            slidesPerView: 2,
+        },
+        950: {
+            slidesPerView: 3,
+        },
+    },
+  });
+
+document.addEventListener('DOMContentLoaded', (event) => {
+  const scrollAmount = 200; // The amount to scroll, adjust as needed
+
+  const scrollContainer = document.querySelector('.scrollable-wrapper');
+  document.querySelector('.left-arrow').addEventListener('click', () => {
+      // Scroll left
+      scrollContainer.scrollLeft -= scrollAmount;
+  });
+
+  document.querySelector('.right-arrow').addEventListener('click', () => {
+      // Scroll right
+      scrollContainer.scrollLeft += scrollAmount;
+  });
+});
