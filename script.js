eeb2f36dc35ef8a59bf66db7541308393ed5f3c5
Wojtek <167181464+Hicior@users.noreply.github.com>
2024-05-08 11:03:41 +0200

    Update script.js

--- script.js
+++ script.js
@@ -40,3 +40,34 @@ function initSwiper() {
 
 // Wait for the swiper container to be available in the DOM before initializing Swiper
 waitForElement('.slide-content', initSwiper);
+
+// BACK TO TOP
+function waitForElement(selector, callback) {
+  var element = document.querySelector(selector);
+  if (element) {
+    callback(element);
+  } else {
+    setTimeout(function() { waitForElement(selector, callback); }, 500);
+  }
+}
+
+// Funkcja, która będzie inicjować Twój skrypt z jQuery
+function initjQueryDependentScripts() {
+  $(window).scroll(function() {
+    if ($(window).scrollTop() > 800) {
+      $('button.back-to-top').addClass('show');
+    } else {
+      $('button.back-to-top').removeClass('show');
+    }
+  });
+
+  $('button.back-to-top').click(function() {
+    $('html, body').animate({
+      scrollTop: 0
+    }, 600);
+    return false;
+  });
+}
+
+// Czekaj na załadowanie biblioteki jQuery przed uruchomieniem skryptów zależnych od jQuery
+waitForElement('script[src*="jquery"]', initjQueryDependentScripts);
